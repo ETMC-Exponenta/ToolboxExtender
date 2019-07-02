a42e1899d898cc19f6fc401075ec4965a3d7df62
Pavel Roslovets <roslovets@exponenta.ru>
2019-07-02 12:01:51 +0300

    Script: dev_on with echo

--- dev_on.m
+++ dev_on.m
@@ -1,5 +1,6 @@
 clear all
 dev = ToolboxDev
+fprintf('Dev: %s v%s\n(%s)\n', dev.ext.name, dev.vp, dev.ext.root)
 % to deploy run: dev.deploy(v) i.e. dev.deploy('0.1.1')
 % to build run dev.build or dev.build(v)
 % to push new version to GitHub: dev.push
