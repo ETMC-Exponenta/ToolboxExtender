b87bc4cbf739825af309edfdb86e56f33a8d818e
roslovets <roslovets@exponenta.ru>
2019-07-02 09:15:16 +0300

    script: install example

--- install.m
+++ install.m
@@ -1,1 +1,3 @@
-open('ToolboxExtender.mltbx');
+matlab.addons.install('MATLAB-Course-for-Educators.mltbx');
+disp('MATLAB Course For Educators has been installed');
+% Add post-install commands below
--- installweb.m
+++ installweb.m
@@ -1,7 +1,10 @@
-instURL = 'https://api.github.com/repos/ETMC-Exponenta/ToolboxExtender';
+instURL = 'https://api.github.com/repos/ETMC-Exponenta/MATLAB-Course-for-Educators';
 [~, instName] = fileparts(instURL);
 instRes = webread(instURL + "/releases/latest");
 fprintf('Downloading %s %s\n', instName, instRes.name);
 websave(instRes.assets.name, instRes.assets.browser_download_url);
-open(instRes.assets.name)
+disp('Installing...')
+matlab.addons.install(instRes.assets.name);
 clear instURL instRes instName
+disp('Installation complete!')
+% Add post-install commands below
